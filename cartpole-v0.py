83ac4e2fbc9e981473399f4e8f039abf9f8e1890
yacoubb <yacoub.ahmedy@gmail.com>
2019-09-29 21:20:31 +0100

    Fixed cartpole rl implementation
    A couple of variable name mismatches caused the previous push to not work at all. After renaming 'pop' to 'population' the population of networks now actually evolve towards an optimal solution.

--- cartpole-v0.py
+++ cartpole-v0.py
@@ -22,11 +22,21 @@ def calc_fitness(network, env_name):
 
 
 def cartpole_rl(
-    population_size, generation_count, pareto_param=0.05, mutation_rate=0.1
+    population_size,
+    generation_count,
+    pareto_param=1,
+    mutation_rate=0.09,
+    stochastic_repeats=20,
 ):
-    # all network_shape, activations, mutation rate and population size are parameters that have to be tuned
+    # population size: the number of neural networks competing in each generation
+    # generation_count: the number of generations to simulate and mutate for
+    # pareto_param: the parameter used to control the distribution from which successful networks are selected and bred
+    # mutation rate: the distribution variance by which network weights and biases are randomly adjusted during mutation
+    # stochastic repeats: the number of repeat simulations to run each network on, to ensure networks don't just get lucky
+
     network_shape = [4, 2]
     activations = ["Sigmoid"]
+    # all of the above are hyperparameters that will need tuning
 
     population = []
     # initialise population_size different neural networks
@@ -41,7 +51,11 @@ def cartpole_rl(
 
         # for every member of the population, see how it performs at the cartpole task
         for i in range(population_size):
-            fitnesses[i] += calc_fitness(population[i], "CartPole-v1")
+            for j in range(stochastic_repeats):
+                fitnesses[i] += (
+                    calc_fitness(population[i], "CartPole-v1") / stochastic_repeats
+                )
+            fitnesses[i] = int(fitnesses[i])
 
         # order the networks in a list based off of their fitnesses
         # reverse since we want highest fitness first
@@ -51,29 +65,30 @@ def cartpole_rl(
 
         # split the ordered list up into fitnesses and population
         ord_fitness, ord_pop = list(zip(*ordered_networks))
-        print(f"max fitness: {ord_fitness[0]} avg fitness: {np.mean(ord_fitness)}")
+        print(
+            f"max fitness: {ord_fitness[0]} avg fitness: {sum(ord_fitness) / population_size}"
+        )
         # now select networks to survive from the ordered list using the pareto distribution
         # https://en.wikipedia.org/wiki/Pareto_distribution
         # we first create a list of indices from the pareto distribution
-        new_pop = []
-        indices = list(
-            np.random.pareto(pareto_param * population_size, population_size)
-        )
+        next_generation = []
+        indices = list(np.random.pareto(pareto_param, population_size))
         indices = list(map(lambda x: int(x) % population_size, indices))
         # select the networks chosen by the pareto distribution
         for i in range(population_size):
             # create a copy of the network
-            mutation = population[indices[i]].copy()
+            mutation = ord_pop[indices[i]].copy()
             # and mutate
             for l in range(len(mutation.weights)):
                 delta_w = np.random.normal(0, mutation_rate, mutation.weights[l].shape)
                 delta_b = np.random.normal(0, mutation_rate, mutation.biases[l].shape)
                 mutation.weights[l] += delta_w
                 mutation.biases[l] += delta_b
-            new_pop.append(mutation)
-        pop = new_pop
+            next_generation.append(mutation)
+        population = next_generation
 
 
 if __name__ == "__main__":
+    np.random.seed(4)
     cartpole_rl(100, 50)
 
