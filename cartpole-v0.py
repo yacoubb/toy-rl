57b729a2d9ee3ea0512b3597c8dc1dc9c4e007c3
yacoubb <yacoub.ahmedy@gmail.com>
2019-10-01 16:54:57 +0100

    Reinforcement learning abstraction
    Reinforcement learning handler now abstracted to a seperate file and function. Now you can call the method with specified hyperparameters, fitness functions and environment names.

--- cartpole-v0.py
+++ cartpole-v0.py
@@ -1,104 +1,43 @@
 import gym
 import numpy as np
-from toy_nn.toy_nn.nn import NeuralNetwork
+from reinforcement import neuro_evolution_rl
 
 
-def calc_fitness(network, env_name, seed, render=False):
+def calc_fitness(network, env_name, seed, stochastic_repeats, render=False):
     # initalise the environment and fitness
     env = gym.make(env_name)
-    env.seed(seed)
     fitness = 0
-    observation = env.reset()
-    for t in range(1000):
-        if render:
-            env.render()
-        # toy-nn doesnt handle having squeezed shapes like (3,) but it handles (3,1) fine
-        observation = np.expand_dims(observation, -1)
-        prediction = network.predict(observation)
-        action = np.argmax(prediction)
-        observation, reward, done, info = env.step(action)
-        fitness += reward
-        if done:
-            break
+    for i in range(stochastic_repeats):
+        env.seed(seed + i * 298310)
+        observation = env.reset()
+        for t in range(1000):
+            if render:
+                env.render()
+            # toy-nn doesnt handle having squeezed shapes like (3,) but it handles (3,1) fine
+            observation = np.expand_dims(observation, -1)
+            prediction = network.predict(observation)
+            action = np.argmax(prediction)
+            observation, reward, done, info = env.step(action)
+            fitness += reward / stochastic_repeats
+            if done:
+                break
     env.close()
     return fitness
 
 
-def cartpole_rl(
-    population_size,
-    generation_count,
-    pareto_param=1,
-    mutation_rate=0.1,
-    stochastic_repeats=20,
-    render_best_rollout=False
-):
-    # population size: the number of neural networks competing in each generation
-    # generation_count: the number of generations to simulate and mutate for
-    # pareto_param: the parameter used to control the distribution from which successful networks are selected and bred
-    # mutation rate: the distribution variance by which network weights and biases are randomly adjusted during mutation
-    # stochastic repeats: the number of repeat simulations to run each network on, to ensure networks don't just get lucky
-
-    network_shape = [4, 2]
-    activations = ["Sigmoid"]
-    # all of the above are hyperparameters that will need tuning
-
-    population = []
-    # initialise population_size different neural networks
-    # remember the weights are initalised randomly
-    for i in range(population_size):
-        population.append(NeuralNetwork(network_shape, activations))
-
-    for e in range(generation_count):
-        # print(f"starting generation {e}")
-        # initialise every population member's fitness to 0
-        fitnesses = [0 for i in range(population_size)]
-
-        # for every member of the population, see how it performs at the cartpole task
-        for i in range(population_size):
-            for j in range(stochastic_repeats):
-                # for each rollout, create a unique but reproduceable seed
-                # by tying environment seed to np.random, the entire set of rollouts all depend on the inital starting seed and hyperparams
-                env_seed = e * population_size + i * stochastic_repeats + stochastic_repeats + int(np.random.randint(0, 5000))
-                fitnesses[i] += calc_fitness(population[i], "CartPole-v1", env_seed) / stochastic_repeats
-            fitnesses[i] = int(fitnesses[i])
-
-        # order the networks in a list based off of their fitnesses
-        # reverse since we want highest fitness first
-        ordered_networks = sorted(
-            list(zip(fitnesses, population)), key=lambda x: x[0], reverse=True
-        )
-
-        # split the ordered list up into fitnesses and population
-        ord_fitness, ord_pop = list(zip(*ordered_networks))
-        print(
-            f"max fitness: {ord_fitness[0]} avg fitness: {sum(ord_fitness) / population_size}"
-        )
-
-        if render_best_rollout:
-            calc_fitness(ord_pop[0], 'CartPole-v1', np.random.randint(0, 19134234), True)
-
-        # now select networks to survive from the ordered list using the pareto distribution
-        # https://en.wikipedia.org/wiki/Pareto_distribution
-        # we first create a list of indices from the pareto distribution
-        next_generation = []
-        indices = list(np.random.pareto(pareto_param, population_size))
-        indices = list(map(lambda x: int(x) % population_size, indices))
-        # select the networks chosen by the pareto distribution
-        for i in range(population_size):
-            # create a copy of the network
-            mutation = ord_pop[indices[i]].copy()
-            # and mutate
-            for l in range(len(mutation.weights)):
-                delta_w = np.random.normal(0, mutation_rate, mutation.weights[l].shape)
-                delta_b = np.random.normal(0, mutation_rate, mutation.biases[l].shape)
-                mutation.weights[l] += delta_w
-                mutation.biases[l] += delta_b
-            next_generation.append(mutation)
-        population = next_generation
-
-
 if __name__ == "__main__":
-    args = {'population_size':100, 'generation_count':50, 'pareto_param':0.5, 'mutation_rate':0.05, 'stochastic_repeats':1, 'render_best_rollout':True}
+    args = {
+        "environment_name": "CartPole-v1",
+        "network_shape": [4, 2],
+        "network_activations": ["Sigmoid"],
+        "population_size": 100,
+        "generation_count": 50,
+        "fitness_func": calc_fitness,
+        "pareto_param": 0.5,
+        "mutation_rate": 0.05,
+        "stochastic_repeats": 20,
+        "render_best_rollout": True,
+    }
     print(args)
-    cartpole_rl(**args)
-
+    np.random.seed(4)
+    neuro_evolution_rl(**args)
---
+++ reinforcement.py
@@ -0,0 +1,79 @@
+import gym
+import numpy as np
+from toy_nn.toy_nn.nn import NeuralNetwork
+
+
+def neuro_evolution_rl(
+    environment_name,
+    network_shape,
+    network_activations,
+    population_size,
+    generation_count,
+    fitness_func,
+    pareto_param=1,
+    mutation_rate=0.1,
+    stochastic_repeats=20,
+    render_best_rollout=False,
+):
+    # all of the above are hyperparameters that will need tuning
+    # fitness func: a function that takes in {network, env_name, seed, stochastic_repeats} and produces a fitness for the network
+    # pareto_param: the parameter used to control the distribution from which successful networks are selected and bred
+    # mutation rate: the distribution variance by which network weights and biases are randomly adjusted during mutation
+    # stochastic repeats: the number of repeat simulations to run each network on, to ensure networks don't just get lucky
+
+    population = []
+    # initialise population_size different neural networks
+    # remember the weights are initalised randomly
+    # networks are initialised using np.random, seed np.random for reproduceability
+    for i in range(population_size):
+        population.append(NeuralNetwork(network_shape, network_activations))
+
+    for e in range(generation_count):
+        # initialise every population member's fitness to 0
+        fitnesses = [0 for i in range(population_size)]
+
+        # for every member of the population, see how it performs at the selected task
+        for i in range(population_size):
+            # for each rollout, create a unique but reproduceable seed
+            env_seed = i * population_size
+            fitnesses[i] += fitness_func(
+                population[i], environment_name, env_seed, stochastic_repeats
+            )
+            fitnesses[i] = int(fitnesses[i])
+
+        # order the networks in a list based off of their fitnesses
+        # reverse since we want highest fitness first
+        ordered_networks = sorted(
+            list(zip(fitnesses, population)), key=lambda x: x[0], reverse=True
+        )
+
+        # split the ordered list up into fitnesses and population
+        ord_fitness, ord_pop = list(zip(*ordered_networks))
+        print(
+            f"max fitness: {ord_fitness[0]} avg fitness: {sum(ord_fitness) / population_size}"
+        )
+
+        if render_best_rollout:
+            fitness_func(
+                ord_pop[0], environment_name, np.random.randint(0, 19134234), 1, True
+            )
+
+        # now select networks to survive from the ordered list using the pareto distribution
+        # https://en.wikipedia.org/wiki/Pareto_distribution
+        # we first create a list of indices from the pareto distribution
+        next_generation = []
+        indices = list(np.random.pareto(pareto_param, population_size))
+        indices = list(map(lambda x: int(x) % population_size, indices))
+        # select the networks chosen by the pareto distribution
+        for i in range(population_size):
+            # create a copy of the network
+            mutation = ord_pop[indices[i]].copy()
+            # and mutate
+            for l in range(len(mutation.weights)):
+                delta_w = np.random.normal(0, mutation_rate, mutation.weights[l].shape)
+                delta_b = np.random.normal(0, mutation_rate, mutation.biases[l].shape)
+                mutation.weights[l] += delta_w
+                mutation.biases[l] += delta_b
+            next_generation.append(mutation)
+        population = next_generation
+
